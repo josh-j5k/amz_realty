a4405ff8d8fe1a17d99c2d3072da417d985f2ff9
josh-j5k <joshdejong5k@gmail.com>
2023-10-05 18:34:30 -0700

    auth done

--- app/Http/Controllers/Auth/AuthenticatedWithSocialsController.php
+++ app/Http/Controllers/Auth/AuthenticatedWithSocialsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Carbon\Traits\Timestamp;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Session;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthenticatedWithSocialsController extends Controller
@@ -21,27 +24,29 @@ class AuthenticatedWithSocialsController extends Controller
         $socialiteUser = Socialite::driver($provider)->user();
 
 
-        $finduser_by_id = User::where('provider_id', $socialiteUser->getId());
-        $finduser_by_email = User::where('email', $socialiteUser->getEmail());
-        if ($finduser_by_id) {
-            Auth::login($finduser_by_id);
-
+        // $finduser_by_id = ->first();
+        // $finduser_by_email = DB::table('users')->->first();
+        if (User::where('provider_id', $socialiteUser->getId())->exists()) {
+            $user_arr = User::where('provider_id', $socialiteUser->getId())->first();
+            Auth::login($user_arr);
+            Session::regenerate();
             return redirect()->intended(RouteServiceProvider::HOME);
-        } else if ($finduser_by_email) {
-            Auth::login($finduser_by_email);
+        } else if (User::where('email', $socialiteUser->getEmail())->exists()) {
+            $user_arr = User::where('email', $socialiteUser->getEmail())->first();
+            Auth::login($user_arr);
+            Session::regenerate();
             return redirect()->intended(RouteServiceProvider::HOME);
         } else {
 
             $user = User::updateOrCreate([
 
                 'name' => $socialiteUser->getName(),
-
                 'email' => $socialiteUser->getEmail(),
-
                 'provider_id' => $socialiteUser->getId(),
                 'provider' => $provider
 
             ]);
+            $user->markEmailAsVerified();
             Auth::login($user);
 
             return redirect()->intended(RouteServiceProvider::HOME);
